834454d1b39308277c01dd8a0a06220741609ebe
rahmat <rahmatbachtiar64@gmail.com>
2025-11-04 09:24:00 +0700

    minor change

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,11 @@ use App\Http\Controllers\Admin\ProductCategoryController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\ProjectController;
 use App\Http\Controllers\Admin\UserController;
-use App\Http\Controllers\website\AboutController;
-use App\Http\Controllers\website\ContactController as WebsiteContactController;
-use App\Http\Controllers\website\HomeController;
-use App\Http\Controllers\website\ProductController as WebsiteProductController;
-use App\Http\Controllers\website\ProjectController as WebsiteProjectController;
+use App\Http\Controllers\Website\AboutController;
+use App\Http\Controllers\Website\ContactController as WebsiteContactController;
+use App\Http\Controllers\Website\HomeController;
+use App\Http\Controllers\Website\ProductController as WebsiteProductController;
+use App\Http\Controllers\Website\ProjectController as WebsiteProjectController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('web-home');
