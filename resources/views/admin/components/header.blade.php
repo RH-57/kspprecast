895ad12b956c0f22d22aedde0b92c1264b193ce6
rahmat <rahmatbachtiar64@gmail.com>
2025-11-21 09:45:02 +0700

    Minor change

--- resources/views/admin/components/header.blade.php
+++ resources/views/admin/components/header.blade.php
@@ -3,7 +3,7 @@
     <div class="d-flex align-items-center justify-content-between">
       <a href="{{route('dashboard.index')}}" class="logo d-flex align-items-center">
         <img src="assets/img/logo.png" alt="">
-        <span class="d-none d-lg-block">KSPKontraktor</span>
+        <span class="d-none d-lg-block">KSP Precast</span>
       </a>
       <i class="bi bi-list toggle-sidebar-btn"></i>
     </div><!-- End Logo -->
