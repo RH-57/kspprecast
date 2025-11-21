ef93f8a48fdeaef81b35718fde8d61f94f08910f
rahmat <rahmatbachtiar64@gmail.com>
2025-11-21 08:28:17 +0700

    Minor change

--- app/Http/Controllers/admin/DashboardController.php
+++ app/Http/Controllers/admin/DashboardController.php
@@ -2,15 +2,10 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Models\Message;
-use App\Models\Post;
 use App\Models\Product;
 use App\Models\ProductCategory;
-use App\Models\Project;
-use App\Models\Visitor;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class DashboardController
 {
